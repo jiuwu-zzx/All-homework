d2097a3ce7f48bad90955231be6f6754c9fced0a
jiuwu-zzx <2627889552@qq.com>
2021-07-08 10:47:46 +0800

    lab2 completed by iterative solution version-3 dcatenate:if(A==null)

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,10 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A==null){
+            A=B;
+            return A;
+        }
         IntList C=A;
         while(C.rest!=null){
             C=C.rest;
